dfbb9c7805394c3fbb220813b926fe2f71569e14
Quoc Anh <52539949+qabk@users.noreply.github.com>
2022-08-18 13:49:45 +0700

    fix padding, colors

--- lib/src/ui/login/login_screen.dart
+++ lib/src/ui/login/login_screen.dart
@@ -32,94 +32,99 @@ class LoginScreen extends StatelessWidget {
               icon: const Icon(Icons.settings))
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset("assets/images/tom.jpg"),
-            ),
-            TextInputField(
-              prefixIcon: const Icon(Icons.email),
-              type: TextInputType.emailAddress,
-              text: "Enter your email",
-              controller: _emailController,
-            ),
-            PassWordField(
-              prefixIcon: const Icon(Icons.lock),
-              hintText: "Enter your password",
-              passwordController: _passwordController,
-            ),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                    onPressed: () {},
-                    child: const Text("Forgot your password?"))),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                    onPressed: () {},
-                    child: const Text("Dont have an account? Sign up here"))),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: BlocListener<LogInBloc, LogInState>(
-                listener: (context, state) {
-                  switch (state.logInStatus) {
-                    case LogInStatus.loading:
-                      {
-                        log("loading");
-                      }
-                      break;
-                    case LogInStatus.loaded:
-                      {
-                        log("loaded");
-                        Navigator.pushNamed(context, '/conversations');
-                      }
-                      break;
-                    case LogInStatus.error:
-                      {
-                        log("error");
-                      }
-                      break;
-                    default:
-                      {
-                        log("default");
-                      }
-                      break;
-                  }
-                },
-                child: InkWell(
-                  onTap: () {
-                    context.read<LogInBloc>().add(LogInSubmitEvent(
-                        email: _emailController.text,
-                        password: _passwordController.text));
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("assets/images/tom.jpg"),
+              ),
+              TextInputField(
+                prefixIcon: const Icon(Icons.email),
+                type: TextInputType.emailAddress,
+                text: "Enter your email",
+                controller: _emailController,
+              ),
+              PassWordField(
+                prefixIcon: const Icon(Icons.lock),
+                hintText: "Enter your password",
+                passwordController: _passwordController,
+              ),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Text("Forgot your password?"))),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/sign_up');
+                      },
+                      child: const Text("Dont have an account? Sign up here"))),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: BlocListener<LogInBloc, LogInState>(
+                  listener: (context, state) {
+                    switch (state.logInStatus) {
+                      case LogInStatus.loading:
+                        {
+                          log("loading");
+                        }
+                        break;
+                      case LogInStatus.loaded:
+                        {
+                          log("loaded");
+                          Navigator.pushNamed(context, '/conversations');
+                        }
+                        break;
+                      case LogInStatus.error:
+                        {
+                          log("error");
+                        }
+                        break;
+                      default:
+                        {
+                          log("default");
+                        }
+                        break;
+                    }
                   },
-                  child: Container(
-                    width: size.width,
-                    height: size.height * 0.05,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      gradient: LinearGradient(
-                          colors: [
-                            Color(0xFF3366FF),
-                            Color(0xFF00CCFF),
-                          ],
-                          begin: FractionalOffset(0.0, 0.0),
-                          end: FractionalOffset(1.0, 0.0),
-                          stops: [0.0, 1.0],
-                          tileMode: TileMode.clamp),
+                  child: InkWell(
+                    onTap: () {
+                      context.read<LogInBloc>().add(LogInSubmitEvent(
+                          email: _emailController.text,
+                          password: _passwordController.text));
+                    },
+                    child: Container(
+                      width: size.width,
+                      height: size.height * 0.05,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        gradient: LinearGradient(
+                            colors: [
+                              Color(0xFF3366FF),
+                              Color(0xFF00CCFF),
+                            ],
+                            begin: FractionalOffset(0.0, 0.0),
+                            end: FractionalOffset(1.0, 0.0),
+                            stops: [0.0, 1.0],
+                            tileMode: TileMode.clamp),
+                      ),
+                      child: const Center(
+                          child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 23),
+                      )),
                     ),
-                    child: const Center(
-                        child: Text(
-                      "Login",
-                      style: TextStyle(fontSize: 23),
-                    )),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/src/ui/sign_up/sign_up_info.dart
+++ lib/src/ui/sign_up/sign_up_info.dart
@@ -43,79 +43,82 @@ class SignUpScreen extends StatelessWidget {
                   title: const Text("Sign up"),
                   centerTitle: true,
                 ),
-                body: SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      // ignore: prefer_const_constructors
-                      AvatarPicker(),
-                      TextInputField(
-                          text: "Enter your name",
-                          type: TextInputType.phone,
-                          prefixIcon: const Icon(Icons.text_fields),
-                          controller: _nameController),
-                      TextInputField(
-                          text: "Enter your Address",
-                          type: TextInputType.name,
-                          prefixIcon: const Icon(Icons.email),
-                          controller: _addressController),
-                      TextInputField(
-                          text: "Enter your phone number",
-                          type: TextInputType.phone,
-                          prefixIcon: const Icon(Icons.phone),
-                          controller: _phoneController),
-                      BlocListener<InfoChangesBloc, InfoChangesState>(
-                        listener: (context, state) {
-                          if (state.changesStage == ChangesStage.success) {
-                            Navigator.pushNamed(
-                              context,
-                              '/conversations',
-                            );
-                          }
-                        },
-                        child: InkWell(
-                          onTap: () {
-                            final bytes = context
-                                .read<ImageCubit>()
-                                .state!
-                                .readAsBytesSync();
-
-                            context.read<InfoChangesBloc>().add(
-                                InfoAddNewUserEvent(
-                                    uid: state.message,
-                                    name: _nameController.text,
-                                    email: args["email"]!,
-                                    password: args["password"]!,
-                                    phoneNumber: _phoneController.text,
-                                    address: _addressController.text,
-                                    base64Image: base64.encode(bytes)));
+                body: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        // ignore: prefer_const_constructors
+                        AvatarPicker(),
+                        TextInputField(
+                            text: "Enter your name",
+                            type: TextInputType.phone,
+                            prefixIcon: const Icon(Icons.text_fields),
+                            controller: _nameController),
+                        TextInputField(
+                            text: "Enter your Address",
+                            type: TextInputType.name,
+                            prefixIcon: const Icon(Icons.email),
+                            controller: _addressController),
+                        TextInputField(
+                            text: "Enter your phone number",
+                            type: TextInputType.phone,
+                            prefixIcon: const Icon(Icons.phone),
+                            controller: _phoneController),
+                        BlocListener<InfoChangesBloc, InfoChangesState>(
+                          listener: (context, state) {
+                            if (state.changesStage == ChangesStage.success) {
+                              Navigator.pushNamed(
+                                context,
+                                '/conversations',
+                              );
+                            }
                           },
-                          child: Container(
-                              height: size.height * 0.05,
-                              width: size.width * 0.4,
-                              decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20)),
-                                gradient: LinearGradient(
-                                    colors: [
-                                      Color(0xFF3366FF),
-                                      Color(0xFF00CCFF),
-                                    ],
-                                    begin: FractionalOffset(0.0, 0.0),
-                                    end: FractionalOffset(1.0, 0.0),
-                                    stops: [0.0, 1.0],
-                                    tileMode: TileMode.clamp),
-                              ),
-                              child: const Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Center(
-                                    child: Text(
-                                  "Submit",
-                                  style: TextStyle(fontSize: 16),
+                          child: InkWell(
+                            onTap: () {
+                              final bytes = context
+                                  .read<ImageCubit>()
+                                  .state!
+                                  .readAsBytesSync();
+
+                              context.read<InfoChangesBloc>().add(
+                                  InfoAddNewUserEvent(
+                                      uid: state.message,
+                                      name: _nameController.text,
+                                      email: args["email"]!,
+                                      password: args["password"]!,
+                                      phoneNumber: _phoneController.text,
+                                      address: _addressController.text,
+                                      base64Image: base64.encode(bytes)));
+                            },
+                            child: Container(
+                                height: size.height * 0.05,
+                                width: size.width * 0.4,
+                                decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
+                                  gradient: LinearGradient(
+                                      colors: [
+                                        Color(0xFF3366FF),
+                                        Color(0xFF00CCFF),
+                                      ],
+                                      begin: FractionalOffset(0.0, 0.0),
+                                      end: FractionalOffset(1.0, 0.0),
+                                      stops: [0.0, 1.0],
+                                      tileMode: TileMode.clamp),
+                                ),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Center(
+                                      child: Text(
+                                    "Submit",
+                                    style: TextStyle(fontSize: 16),
+                                  )),
                                 )),
-                              )),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               );
